68dd8b37e3d8abf1911c13ffbeb1621192f18038
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-01-08 18:02:08 +0000

    Iniital Commit

---
+++ MissionPlanner.Aerobridge/AerobridgePlugin.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * This is a sample plugin frame work for Mission Planner 
+ * 
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MissionPlanner.Utilities;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+using MissionPlanner.Controls;
+
+namespace MissionPlanner.AerobridgeConnector
+{
+    public class AerobridgePlugin : MissionPlanner.Plugin.Plugin
+    {
+
+        //Additional variables 
+        MissionPlanner.Controls.pingButton button;
+
+        public override string Name
+        {
+            get { return "Misson Planner Aerobridge Plugin"; }
+        }
+
+        public override string Version
+        {
+            get { return "1.0" }
+        }
+
+        public override string Author
+        {
+            get { return "Openskies Aerial Technology Ltd."; }
+        }
+
+        public override bool Init()
+        {
+            return true;
+        }
+
+        public override bool Loaded()
+        {
+
+            return true;
+        }
+
+        private bool Instance_ProcessCmdKeyCallback(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+
+            //Add our shortcut (Alt + I) 
+
+            if (keyData == (Keys.Alt | Keys.I))
+            {
+                launch_welcome(this, null);
+                return true;
+            }
+            return false;
+        }
+
+        // Main 
+        void launch_welcome(object sender, EventArgs e)
+        {
+            //Add intro and some warnings
+            CustomMessageBox.Show("This plugin will connect to a Aerobridge Server Instance.\r\n\r\n" +
+                                  "Please make sure you have the proper credentials for Aerobridge first.\r\n", "Welcome to Aerobridge");
+
+
+            if (!Host.cs.connected)
+            {
+                CustomMessageBox.Show("Please connect first!", "Initial paremeter calculator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        public override bool Exit()
+        {
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Mission Planner Plugin for Aerobridge NPNT Server
+
+This is the basic plugin connecting Mission Planner to Aerobridge Server. 
+
+__January 2021__ : This plugin is under heavy development, and not for production use. The installation instructions and user interface **will** change. Manufacturers / Operators: if you want to test / integrate this, join our "Canary program" by registering for our [webinar series](http://webinar.aerobridge.in).
+
+## Installation
+
+For the moment please follow the following steps:
+
+1. Ensure that you have Mission Planner Installed on your PC.
+2. Please copy the `Aerobridge_Plugin.cs` file into the `C:\Program Files (x86)\Mission Planner\plugins` directory.
+3. Open Mission Planner and click `Ctrl + A` to launch the plugin.
+
+## Development
+
+You can open the the `.sln` file in Visual Studio Community Edition for free. If you want to access the Mission Planner Namespace, download the latest solution from [MissionPlanner Releases](https://github.com/ArduPilot/MissionPlanner/releases/) page, open it in Visual Studio Community Edition. Then you can add this solution to your project by following [Microsoft help directions](https://docs.microsoft.com/en-us/sql/ssms/solution/add-an-existing-project-to-a-solution?view=sql-server-ver15)
