0ec6e6c497ad930978944415798470714e118f20
Hrishikesh Ballal <hballal@gmail.com>
2022-02-17 22:29:43 +0000

    Update aerobridge_gcs.py

--- mavsdk-aerobridge/aerobridge_gcs.py
+++ mavsdk-aerobridge/aerobridge_gcs.py
@@ -1,21 +1,30 @@
-
 import asyncio
 from mavsdk import System
-
+from mavsdk.info import InfoError
 async def run():
-    drone = System()
-    # await drone.connect(system_address="tcp://127.0.0.1:5762")
 
-    print("Waiting for drone to connect...")
-    async for state in drone.core.connection_state():
+    # Init the drone
+    vehicle = System()
+    await vehicle.connect(system_address="udp://:14540")
+
+    async for state in vehicle.core.connection_state():
         if state.is_connected:
-            print(f"Drone discovered!")
+            print(f"Vehicle discovered")
             break
-    
-    info = await drone.info.get_identification()
-    print(info)
 
+    got_info = False 
+    while not got_info:
+        try:
+            id_info = await vehicle.info.get_version()
+        except InfoError as infoError:
+            print(infoError)
+        else: 
+            print(id_info)
+            got_info = True
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Start the main function
     asyncio.ensure_future(run())
+
+    # Runs the event loop until the program is canceled with e.g. CTRL-C
     asyncio.get_event_loop().run_forever()
