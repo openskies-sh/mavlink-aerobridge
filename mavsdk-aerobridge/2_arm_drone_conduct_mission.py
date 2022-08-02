b69f5a181f7b5d0691149967cdfcd082d537829e
Hrishikesh Ballal <hballal@gmail.com>
2022-08-02 21:37:07 +0100

    Added RID position data submission

--- mavsdk-aerobridge/2_arm_drone_conduct_mission.py
+++ mavsdk-aerobridge/2_arm_drone_conduct_mission.py
@@ -1,20 +1,60 @@
-import asyncio, time, random
+import asyncio, time
 from mavsdk import System
+import flightblendertools
+from dataclasses import dataclass, asdict
+from typing import Optional
+import arrow, json
+
+@dataclass
+class RIDAircraftPosition:
+  lat: float
+  lng: float
+  alt: float
+  accuracy_h: str
+  accuracy_v: str
+  extrapolated: Optional[bool]
+  pressure_altitude: Optional[float]
+
+@dataclass
+class RIDHeight:
+  distance: float
+  reference: str
+
+
+@dataclass
+class RIDAircraftState:
+  timestamp: str
+  timestamp_accuracy: float
+  speed_accuracy: str
+  position: RIDAircraftPosition
+  operational_status: Optional[str]= None
+  track: Optional[float] = None
+  speed: Optional[float] = None
+  vertical_speed: Optional[float] = None
+  height: Optional[RIDHeight] = None
+
+  def as_dict(self):
+      data = asdict(self)
+      return {key: value for key, value in data.items() if value is not None}
+
 
 async def run():
-
     drone = System()
     await drone.connect(system_address="udp://:14540")
-
     print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
         if state.is_connected:
             print(f"Drone discovered!")
             break
 
+
+    my_credentials = flightblendertools.PassportCredentialsGetter()
+    credentials = my_credentials.get_cached_credentials()
+
+    my_blender_uploader = flightblendertools.BlenderUploader(credentials=credentials)
     start_time = time.time()
     print_mission_progress_task = asyncio.ensure_future(print_mission_progress(drone))
-    position_task=  asyncio.ensure_future(get_position_x_seconds(3, get_drone_position,drone, start_time ))    
+    position_task=  asyncio.ensure_future(get_position_x_seconds(3, get_drone_position,drone, start_time, my_blender_uploader ))    
     running_tasks = [print_mission_progress_task, position_task]
     termination_task = asyncio.ensure_future(observe_is_in_air(drone, running_tasks))
     
@@ -30,11 +70,23 @@ async def print_mission_progress(drone):
               f"{mission_progress.current}/"
               f"{mission_progress.total}")
 
-async def get_position_x_seconds(timeout, get_drone_position, drone, start_time):
+async def get_position_x_seconds(timeout, get_drone_position, drone, start_time, my_blender_uploader):
     while True:
         await asyncio.sleep(timeout)
         position = await get_drone_position(drone, start_time)   
         # send position to Flight Blender
+     
+     
+        position = RIDAircraftPosition(
+                "lat": position['lat'],
+                "lng": position['lng'],
+                "alt": position['altitude'],
+                "accuracy_h": "HAUnkown",
+                "accuracy_v": "VAUnknown",
+                "extrapolated": False)
+        rid_json = RIDAircraftState(timestamp=arrow.now().isoformat(),operational_status="Airborne", position = position, height=RIDHeight(distance = 50.0, reference = "TakeoffLocation"), track=181.69, speed= 4.91, timestamp_accuracy=0.0, speed_accuracy="SA3mps", vertical_speed=0.0)
+        
+        my_blender_uploader.upload_to_blender(rid_json = json.dumps(asdict(rid_json)))
         print(position)
 
 async def observe_is_in_air(drone, running_tasks):
@@ -44,7 +96,6 @@ async def observe_is_in_air(drone, running_tasks):
     async for is_in_air in drone.telemetry.in_air():
         if is_in_air:
             was_in_air = is_in_air
-
         if was_in_air and not is_in_air:
             for task in running_tasks:
                 task.cancel()
@@ -53,7 +104,6 @@ async def observe_is_in_air(drone, running_tasks):
                 except asyncio.CancelledError:
                     pass
             await asyncio.get_event_loop().shutdown_asyncgens()
-
             return
 
 async def get_drone_position( drone, start_time):
---
+++ mavsdk-aerobridge/flightblendertools.py
@@ -0,0 +1,87 @@
+from os import environ as env
+
+from dotenv import load_dotenv, find_dotenv
+import json
+import time
+import requests
+from dataclasses import dataclass, asdict
+from typing import Optional
+from datetime import datetime, timedelta
+from os.path import exists
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+@dataclass
+class RIDOperatorDetails():
+  id: str
+  operator_id: Optional[str]
+  operation_description: Optional[str]
+  serial_number: Optional[str]
+  registration_number: Optional[str]
+  aircraft_type:str = 'Helicopter'
+
+
+class PassportCredentialsGetter():
+    def __init__(self):
+        pass
+
+    def get_cached_credentials(self):  
+        token_exists = exists('token.json')
+        if token_exists:
+            with open('token.json', 'r') as token_file:
+                token_details = token_file.read()
+                token_details = json.loads(token_details)
+                credentials = token_details['credentials']
+        else:               
+            credentials = self.get_write_credentials()
+            with open('token.json', 'w') as token_file:
+                token_file.write(json.dumps(credentials))
+            
+        return credentials
+            
+    def get_write_credentials(self):        
+        payload = {"grant_type":"client_credentials","client_id": env.get('BLENDER_WRITE_CLIENT_ID'),"client_secret": env.get('BLENDER_WRITE_CLIENT_SECRET'),"audience": env.get('BLENDER_AUDIENCE'),"scope": env.get('BLENDER_WRITE_SCOPE')}        
+        
+        url = env.get('PASSPORT_URL') + env.get('PASSPORT_TOKEN_URL')
+        
+        token_data = requests.post(url, data = payload)
+        t_data = token_data.json()
+        
+        return t_data
+
+class BlenderUploader():
+    
+    def __init__(self, credentials):        
+        self.credentials = credentials
+    
+    def upload_to_server(self, rid_json):
+        
+        states = rid_json['current_states']
+        
+        rid_operator_details = RIDOperatorDetails(
+            id="382b3308-fa11-4629-a966-84bb96d3b4db",
+            serial_number='d29dbf50-f411-4488-a6f1-cf2ae4d4237a',
+            operation_description="Medicine Delivery",
+            operator_id='CHE-076dh0dq  ',
+            registration_number='CHE-5bisi9bpsiesw',
+        )
+
+        for state in states: 
+            headers = {"Content-Type":'application/json',"Authorization": "Bearer "+ self.credentials['access_token']}     
+            
+            payload = {"observations":[{"current_states":[state], "flight_details": asdict(rid_operator_details) }]}
+            
+            securl = env.get('BLENDER_RID_FQDN', 'http://localhost:8000/rid/flight_data') # set this to self (Post the json to itself)
+            try:
+                response = requests.put(securl, json = payload, headers = headers)
+                
+            except Exception as e:                
+                print(e)
+            else:
+                if response.status_code == 201:
+                    print("Sleeping 3 seconds..")
+                    time.sleep(3)
+                else: 
+                    print(response.json())
+
