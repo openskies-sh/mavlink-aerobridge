3836adb24331f9ed08c5aa87c72bb12610f47d9b
hrishiballal <hrishikeshballal@yahoo.com>
2023-04-17 19:35:44 +0100

    Update flightblendertools.py

--- mavsdk-aerobridge/flightblendertools.py
+++ mavsdk-aerobridge/flightblendertools.py
@@ -12,22 +12,84 @@ from os.path import exists
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
+from dataclasses import dataclass
+from typing import Optional
+from enum import Enum
 
+SpecificSessionID = str
 @dataclass
 class LatLngPoint:
   lat: float
   lng: float
 
+class Reference1(Enum):
+    W84 = 'W84'
+
+class Units(Enum):
+    M = 'M'
+
+class Category(Enum):
+    EUCategoryUndefined = 'EUCategoryUndefined'
+    Open = 'Open'
+    Specific = 'Specific'
+    Certified = 'Certified'
+
+class Class(Enum):
+    EUClassUndefined = 'EUClassUndefined'
+    Class0 = 'Class0'
+    Class1 = 'Class1'
+    Class2 = 'Class2'
+    Class3 = 'Class3'
+    Class4 = 'Class4'
+    Class5 = 'Class5'
+    Class6 = 'Class6'
+
 @dataclass
-class RIDOperatorDetails():
-  id: str
-  operator_location:LatLngPoint
-  operator_id: Optional[str]
-  operation_description: Optional[str]
-  serial_number: Optional[str]
-  registration_number: Optional[str]
-  
-  aircraft_type:str = 'Helicopter'
+class Altitude:
+    value: float
+    reference: Reference1
+    units: Units
+
+class AltitudeType(Enum):
+    Takeoff = 'Takeoff'
+    Dynamic = 'Dynamic'
+    Fixed = 'Fixed'
+
+@dataclass
+class RIDAuthData:
+    data: Optional[str] = ''
+    format: Optional[int] = 0
+
+@dataclass
+class OperatorLocation:
+    position: LatLngPoint
+    altitude: Optional[Altitude] = None
+    altitude_type: Optional[AltitudeType] = None
+
+@dataclass
+class UASID:
+    specific_session_id: Optional[SpecificSessionID] = None
+    serial_number: Optional[str] = ''
+    registration_id: Optional[str] = ''
+    utm_id: Optional[str] = ''
+
+
+@dataclass
+class UAClassificationEU:
+    category: Optional[Category] = 'EUCategoryUndefined'
+    class_: Optional[Class] = 'EUClassUndefined'
+
+
+
+@dataclass
+class RIDFlightDetails:
+    id: str
+    eu_classification: Optional[UAClassificationEU] = None
+    uas_id: Optional[UASID] = None
+    operator_location: Optional[OperatorLocation] = None
+    auth_data: Optional[RIDAuthData] = None
+    operator_id: Optional[str] = ''
+    operation_description: Optional[str] = ''
 
 
 class PassportCredentialsGetter():
@@ -44,7 +106,6 @@ class PassportCredentialsGetter():
             
     def get_cached_credentials(self):  
         token_exists = exists('token.json')
-        print(token_exists)
         if token_exists:
             with open('token.json', 'r') as token_file:
                 token_details = token_file.read()
@@ -73,14 +134,19 @@ class BlenderUploader():
     def upload_to_blender(self, rid_json):
         states = rid_json['states']
         
-        rid_operator_details = RIDOperatorDetails(
+        uas_id = UASID(registration_id = 'CHE-5bisi9bpsiesw',  serial_number='d29dbf50-f411-4488-a6f1-cf2ae4d4237a',utm_id= '07a06bba-5092-48e4-8253-7a523f885bfe')
+      
+        operator_location = OperatorLocation(position = LatLngPoint(lat = 46.97615311620088,lng = 7.476099729537965))
+        rid_operator_details = RIDFlightDetails(
             id="382b3308-fa11-4629-a966-84bb96d3b4db",
-            serial_number='d29dbf50-f411-4488-a6f1-cf2ae4d4237a',
+            uas_id = uas_id,
             operation_description="Medicine Delivery",
             operator_id='CHE-076dh0dq',
-            registration_number='CHE-5bisi9bpsiesw',            
-            operator_location=  LatLngPoint(lat = 46.97615311620088,lng = 7.476099729537965)
+            eu_classification = 'Class0',            
+            operator_location=  operator_location
         )
+
+
 
         securl = env.get('BLENDER_RID_FQDN', 'http://localhost:8000/flight_stream/set_telemetry') # set this to self (Post the json to itself)
         
